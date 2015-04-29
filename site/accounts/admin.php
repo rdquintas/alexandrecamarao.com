7be42d878eb2603bfd6f1927bd59b362c3bef7f3
Ricardo Quintas <rqcenter-web@example.com>
2015-04-29 10:21:13 +0100

    initial build

---
+++ site/accounts/admin.php
@@ -0,0 +1,9 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: admin
+email: rdquintas@yahoo.com
+password: >
+  $2a$10$NJ.V9HKL1l1S8DC8SveZlOp1JtHeAaA1MqmWv2D8ywb3.0kPqteYm
+language: en
+role: admin
+token: e44315518a8dc6ae308e79ff8c27e0de763d2738
