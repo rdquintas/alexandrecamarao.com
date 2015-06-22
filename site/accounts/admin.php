ba6d78f2da4105fc8fe009ebc8dea0b6dde1bfcf
Ricardo Quintas <rqcenter-web@yahoo.com>
2015-06-22 19:38:16 +0100

    delete kirby

--- site/accounts/admin.php
+++
@@ -1,8 +0,0 @@
-<?php if(!defined('KIRBY')) exit ?>
-
-username: admin
-email: admin@teste.com
-password: >
-  $2a$10$dlA.3bRxJgBBYzfoy3/gAuwilug.Y5XmWfYLaQtDL57PW7/Ql8wtu
-language: en
-role: admin
