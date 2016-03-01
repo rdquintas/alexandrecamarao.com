eeae6c35de12853045162df30ba8c37a077ded50
rdquintas <rqcenter-web@yahoo.com>
2016-03-01 19:56:19 +0000

    fixing sort order descending

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,199 +1,123 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  var _js_libs = [
+    'bower_components/echojs/dist/echo.min.js',
+    'bower_components/superfish/dist/js/hoverIntent.js',
+    'bower_components/superfish/dist/js/superfish.min.js',
+    'user/themes/zrq/js/jquery.hoverdir.js',
+    'user/themes/zrq/js/slidebars.js',
+    'user/themes/zrq/js/magnific-popup.js'
+  ]
 
-    var _js_libs = [
-        "bower_components/echojs/dist/echo.min.js",
-        "bower_components/superfish/dist/js/hoverIntent.js",
-        "bower_components/superfish/dist/js/superfish.min.js",
-        'user/themes/zrq/js/jquery.hoverdir.js',
-        'user/themes/zrq/js/slidebars.js',
-        'user/themes/zrq/js/magnific-popup.js'
-    ];
+  var _js_custom = [
+    'user/themes/zrq/js/app.js'
+  ]
 
-    var _js_custom = [
-        'user/themes/zrq/js/app.js'
-    ];
+  // grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-contrib-jshint')
+  grunt.loadNpmTasks('grunt-contrib-concat')
+  // grunt.loadNpmTasks('grunt-contrib-rename')
+  // grunt.loadNpmTasks('grunt-contrib-cssmin')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  // grunt.loadNpmTasks('grunt-contrib-clean')
+  // grunt.loadNpmTasks('grunt-contrib-connect')
+  // grunt.loadNpmTasks('grunt-contrib-compress')
+  grunt.loadNpmTasks('grunt-contrib-less')
+  // grunt.loadNpmTasks('grunt-contrib-copy')
 
-    // grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    // grunt.loadNpmTasks('grunt-contrib-rename');
-    // grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    // grunt.loadNpmTasks('grunt-contrib-clean');
-    // grunt.loadNpmTasks('grunt-contrib-connect');
-    // grunt.loadNpmTasks('grunt-contrib-compress');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    // grunt.loadNpmTasks('grunt-contrib-copy');
+  // ================================
+  // Task:    default
+  // ================================
+  grunt.registerTask('default', [
+    // 'concat:js_libs',
+    // 'jshint:js_custom',
+    'concat:js_libs',
+    'concat:js_custom',
+    // 'uglify:js_custom',
+    'less:css_custom'
+  // 'copy:purecss',
+  // 'cssmin:css_custom'
+  ])
 
+  // ================================
+  // Task:    prod
+  // ================================    
+  grunt.registerTask('prod', [
+    // 'concat:js_libs',
+    // 'jshint:js_custom',
+    // 'concat:js_custom',
+    // 'uglify:js_custom',
+    // 'libsass:css_custom',
+    // 'copy:purecss',
+    // 'cssmin:css_custom'
+  ])
 
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     // ================================
-    // Task:    default
-    // ================================
-    grunt.registerTask('default', [
-        // 'concat:js_libs',
-        'jshint:js_custom',
-        'concat:js_libs',
-        'concat:js_custom',
-        // 'uglify:js_custom',
-        'less:css_custom'
-        // 'copy:purecss',
-        // 'cssmin:css_custom'
-    ]);
-
-
-    // ================================
-    // Task:    prod
-    // ================================    
-    grunt.registerTask('prod', [
-        // 'concat:js_libs',
-        // 'jshint:js_custom',
-        // 'concat:js_custom',
-        // 'uglify:js_custom',
-        // 'libsass:css_custom',
-        // 'copy:purecss',
-        // 'cssmin:css_custom'
-    ]);
-
-
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        // ================================
-        // WATCH
-        // ================================          
-        watch: {
-            js_custom: {
-                files: _js_custom,
-                tasks: [
-                    'jshint:js_custom',
-                    'concat:js_custom'
-                ],
-                options: {
-                    event: ['all'],
-                    interrupt: true
-                }
-            },
-            css_custom: {
-                files: "user/themes/zrq/less/*.less",
-                tasks: [
-                    'less:css_custom'
-                ],
-                options: {
-                    event: ['all'],
-                    interrupt: true
-                }
-            }
-        },
-
-
-        // ================================
-        // JSHINT
-        // ================================          
-        jshint: {
-            js_custom: {
-                src: _js_custom
-            }
-        },
-
-
-        // ================================
-        // CONCAT
-        // ================================          
-        concat: {
-            js_libs: {
-                src: _js_libs,
-                dest: 'user/themes/zrq/js/libs.dist.js'
-            },
-            js_custom: {
-                src: _js_custom,
-                dest: 'user/themes/zrq/js/app.dist.js'
-            }
-        },
-
-
-        // // ================================
-        // // UGLIFY: JS minify
-        // // ================================  
-        // uglify: {
-        //     js_custom: {
-        //         src: 'js/app.dist.js',
-        //         dest: 'js/app.dist.js'
-        //     }
-        // },
-
-
-        // // ================================
-        // // CSSMIN
-        // // ================================        
-        // cssmin: {
-        //     css_custom: {
-        //         files: {
-        //             'css/style.dist.css': ['css/style.dist.css']
-        //         }
-        //     }
-        // },
-
-
-
-        // ================================
-        // LESS
-        // ================================  
-        less: {
-            css_custom: {
-                options: {
-                    sourceMap: true,
-                    sourceMapFilename: "user/themes/zrq/css/app.dist.css.map",
-                    cleancss: true,
-                    compress: true
-                },
-                files: {
-                    "user/themes/zrq/css/app.dist.css": "user/themes/zrq/less/start_here.less"
-                }
-            }
+    // WATCH
+    // ================================          
+    watch: {
+      js_custom: {
+        files: _js_custom,
+        tasks: [
+          'concat:js_custom'
+        ],
+        options: {
+          event: ['all'],
+          interrupt: true
         }
+      },
+      css_custom: {
+        files: 'user/themes/zrq/less/*.less',
+        tasks: [
+          'less:css_custom'
+        ],
+        options: {
+          event: ['all'],
+          interrupt: true
+        }
+      }
+    },
 
+    // // ================================
+    // // JSHINT
+    // // ================================          
+    // jshint: {
+    //   js_custom: {
+    //     src: _js_custom
+    //   }
+    // },
 
+    // ================================
+    // CONCAT
+    // ================================          
+    concat: {
+      js_libs: {
+        src: _js_libs,
+        dest: 'user/themes/zrq/js/libs.dist.js'
+      },
+      js_custom: {
+        src: _js_custom,
+        dest: 'user/themes/zrq/js/app.dist.js'
+      }
+    },
 
-        // // ================================
-        // // COPY
-        // // ================================  
-        // copy: {
-        //     purecss: {
-        //         src: 'bower_components/pure/pure-min.css',
-        //         dest: 'css/pure-min.css'
-        //     }
-        // },
+    // ================================
+    // LESS
+    // ================================  
+    less: {
+      css_custom: {
+        options: {
+          sourceMap: true,
+          sourceMapFilename: 'user/themes/zrq/css/app.dist.css.map',
+          cleancss: true,
+          compress: true
+        },
+        files: {
+          'user/themes/zrq/css/app.dist.css': 'user/themes/zrq/less/start_here.less'
+        }
+      }
+    }
 
-
-        // // ================================
-        // // CLEAN
-        // // ================================          
-        // clean: ['js/<%= pkg.name %>.js'],
-
-
-        // // ================================
-        // // RENAME
-        // // ================================          
-        // rename: {
-        //     // css: {
-        //     //     files: [{
-        //     //         src: 'css/teste.css',
-        //     //         dest: 'css/teste.min.css'
-        //     //     }]
-        //     // }
-        // },
-
-
-        // // ================================
-        // // CONNECT
-        // // ================================
-        // connect: {
-        //     server: {
-        //         options: {
-        //             port: 8080,
-        //             keepalive: true
-        //         }
-        //     }
-        // }
-
-    });
-};
+  })
+}
--- user/themes/zrq/js/app.js
+++ user/themes/zrq/js/app.js
@@ -1,89 +1,97 @@
-/************************************
- * Superfish navigation menu
- ************************************/
-$(document).ready(function() {
+// Superfish navigation menu
 
-    var zrq = $('.zrq');
-    if (zrq.length === 0) {
-        return;
-    }
+var $ = window.$
 
+$(document).ready(function () {
+  var zrq = $('.zrq')
+  if (zrq.length === 0) {
+    return
+  }
 
-    // clone menu for mobile
-    $("#original-menu ul.sf-menu").clone().appendTo("#nav-menu-mobile");
+  // Sort the images by year descending
+  if ($('.page-artwork').length > 0) {
+    var listitems = $('ul.da-thumbs').children('li').get()
+    listitems.sort(function (a, b) {
+      var compA = parseInt($(a).data('year'))
+      var compB = parseInt($(b).data('year'))
+      console.log('compA: ' + compA)
+      console.log('compB: ' + compB)
+      return (compA > compB) ? -1 : (compA < compB) ? 1 : 0
+    })
 
-    $("#nav-menu-mobile ul").removeClass("sf-menu");
+    var myList = $('ul.da-thumbs')
+    $(myList).html = ''
+    $.each(listitems, function (idx, itm) {
+      myList.append(itm)
+    })
+  }
 
-    // initialize slide menu
-    $.slidebars();
+  // clone menu for mobile
+  $('#original-menu ul.sf-menu').clone().appendTo('#nav-menu-mobile')
 
-    // initialize superfish dropdown menus
-    $('nav#original-menu').superfish({
-        delay: 0,
-        speed: 0,
-        speedOut: 0,
-        cssArrows: false,
-        autoArrows: false // disable generation of arr
-    });
+  $('#nav-menu-mobile ul').removeClass('sf-menu')
 
-    if ($('.page-home').length > 0) {
-        $('header.header').css('background-color', 'rgba(255, 255, 255, 0)');
-        $('#original-menu .sf-menu li').css('background-color', 'rgba(255, 255, 255, 0)');
-        $('#original-menu .sf-menu li a').css('background-color', 'rgba(255, 255, 255, 0)');
-        $('html').css('overflow-y', 'hidden');
-    }
+  // initialize slide menu
+  $.slidebars()
 
-    $('#sb-site').css('overflow-y', 'hidden');
+  // initialize superfish dropdown menus
+  $('nav#original-menu').superfish({
+    delay: 0,
+    speed: 0,
+    speedOut: 0,
+    cssArrows: false,
+    autoArrows: false // disable generation of arr
+  })
 
-    // Initializes the lazy-load library
-    echo.init({
-        offset: 100,
-        throttle: 250,
-        unload: false
-    });
-});
+  if ($('.page-home').length > 0) {
+    $('header.header').css('background-color', 'rgba(255, 255, 255, 0)')
+    $('#original-menu .sf-menu li').css('background-color', 'rgba(255, 255, 255, 0)')
+    $('#original-menu .sf-menu li a').css('background-color', 'rgba(255, 255, 255, 0)')
+    $('html').css('overflow-y', 'hidden')
+  }
 
+  $('#sb-site').css('overflow-y', 'hidden')
 
-/************************************
- * Onscroll event to shrink header
- ************************************/
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 100) {
-        // console.log($(document).scrollTop());
-        $('.header').addClass('smaller');
-    } else {
-        $('.header').removeClass('smaller');
-    }
-});
+  // Initializes the lazy-load library
+  window.echo.init({
+    offset: 100,
+    throttle: 250,
+    unload: false
+  })
+})
 
+// Onscroll event to shrink header
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 100) {
+    // console.log($(document).scrollTop())
+    $('.header').addClass('smaller')
+  } else {
+    $('.header').removeClass('smaller')
+  }
+})
 
-/************************************
- * Dropdown event
- ************************************/
-$("#year").change(function(e) {
-    window.location.href = "?year=" + $(this).val();
-});
+// Dropdown event
+$('#year').change(function (e) {
+  window.location.href = '?year=' + $(this).val()
+})
 
-
-/************************************
- * Initialize Lightbox 
- ************************************/
+// Initialize Lightbox
 $('.da-thumbs').magnificPopup({
-    delegate: 'a', // child items selector, by clicking on it popup will open
-    type: 'image',
-    disableOn: function() {
-        if ($(window).width() < 850) {
-            return false;
-        }
-        return true;
-    },
-    gallery: {
-        // options for gallery
-        enabled: true
-    },
-    image: {
-        // options for image content type
-        titleSrc: 'title'
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+  disableOn: function () {
+    if ($(window).width() < 850) {
+      return false
     }
-    // other options
-});
+    return true
+  },
+  gallery: {
+    // options for gallery
+    enabled: true
+  },
+  image: {
+    // options for image content type
+    titleSrc: 'title'
+  }
+// other options
+})
